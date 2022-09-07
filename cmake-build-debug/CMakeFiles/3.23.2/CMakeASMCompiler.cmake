2f6bcf69af1974777c1085cc9a85c4a14f7416e7
Henryjackey <1666796995@qq.com>
2022-09-07 11:07:14 +0800

    first

---
+++ cmake-build-debug/CMakeFiles/3.23.2/CMakeASMCompiler.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_ASM_COMPILER "E:/exploit tools/cubemx/gcc/gcc-arm-none-eabi/bin/arm-none-eabi-gcc.exe")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "arm-none-eabi-ar")
+set(CMAKE_ASM_COMPILER_AR "E:/exploit tools/cubemx/gcc/gcc-arm-none-eabi/bin/arm-none-eabi-gcc-ar.exe")
+set(CMAKE_RANLIB "E:/exploit tools/cubemx/gcc/gcc-arm-none-eabi/bin/arm-none-eabi-ranlib.exe")
+set(CMAKE_ASM_COMPILER_RANLIB "E:/exploit tools/cubemx/gcc/gcc-arm-none-eabi/bin/arm-none-eabi-gcc-ranlib.exe")
+set(CMAKE_LINKER "E:/exploit tools/cubemx/gcc/gcc-arm-none-eabi/bin/arm-none-eabi-ld.exe")
+set(CMAKE_MT "")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "GNU")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
---
+++ cmake-build-debug/CMakeFiles/LED00.elf.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for LED00.elf.
+# This may be replaced when dependencies are built.
